87d9121db616b151d225d1eddecf489ddb6b09ef
Matt McCullough <mmccullough6899@gmail.com>
2025-09-21 13:59:41 -0400

    Rename method

--- src/io/SongModifier.java
+++ src/io/SongModifier.java
@@ -117,7 +117,7 @@ public class SongModifier {
 
             //modify song
 
-            if (checkInvalidSize(dspNibbleCount, nibbleCount)) return;
+            if (isInvalidSize(dspNibbleCount, nibbleCount)) return;
 
             long newSampleRateOffset = thisHeaderOffs + 4;
             long newNibbleCount = thisHeaderOffs + 8;
@@ -154,7 +154,7 @@ public class SongModifier {
         }
     }
 
-    private static boolean checkInvalidSize(byte[] dspNibbleCount, long nibbleCount) {
+    private static boolean isInvalidSize(byte[] dspNibbleCount, long nibbleCount) {
         //size check
         long newDSPSize = ((long) ((dspNibbleCount[0] & 0xFF) << 24)
                 | ((dspNibbleCount[1] & 0xFF) << 16)
