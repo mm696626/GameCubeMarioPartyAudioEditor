f420884a3edcca1640139ad577d3da6fb6ab16d4
Matt McCullough <mmccullough6899@gmail.com>
2025-09-21 13:45:06 -0400

    Remove unused methods

--- src/io/BinaryIO.java
+++ src/io/BinaryIO.java
@@ -1,13 +1,9 @@
 package io;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class BinaryIO {
-
-    // === READ FROM RandomAccessFile ===
-    public static int readU8BE(RandomAccessFile raf) throws IOException {
-        return raf.readUnsignedByte();
-    }
 
     public static int readU16BE(RandomAccessFile raf) throws IOException {
         int b0 = raf.readUnsignedByte();
@@ -21,22 +17,5 @@ public class BinaryIO {
         long b2 = raf.readUnsignedByte();
         long b3 = raf.readUnsignedByte();
         return (b0 << 24) | (b1 << 16) | (b2 << 8) | b3;
-    }
-
-    // === WRITE TO OutputStream ===
-    public static void writeU8BE(OutputStream out, int val) throws IOException {
-        out.write(val & 0xFF);
-    }
-
-    public static void writeU16BE(OutputStream out, int val) throws IOException {
-        out.write((val >> 8) & 0xFF);
-        out.write(val & 0xFF);
-    }
-
-    public static void writeU32BE(OutputStream out, long val) throws IOException {
-        out.write((int) ((val >> 24) & 0xFF));
-        out.write((int) ((val >> 16) & 0xFF));
-        out.write((int) ((val >> 8) & 0xFF));
-        out.write((int) (val & 0xFF));
     }
 }
